20e9ac9b22d89ec60db69101edad8abc1ec36870
Chu Quang Huy <huy123nn69hz@gmail.com>
2022-11-26 02:28:16 +0700

    adsdqweqew

--- src/components/Body/Input.tsx
+++ src/components/Body/Input.tsx
@@ -7,18 +7,18 @@ import { IoIosAdd } from "react-icons/io";
 
 const Input = React.forwardRef<HTMLInputElement>((props, ref) => {
     const {item, addItem, onChangeHandle, scrollFocus, focusInput} = useContext(myContext);
-    useEffect(() => {
+    /* useEffect(() => {
         scrollFocus();
-    })
+    }) */
 	return (
 		<div className="flex items-center">
-            <form onSubmit={e => {addItem(e), scrollFocus()}}>
+            <form onSubmit={e => {addItem(e)/* , scrollFocus() */}}>
                 <input ref={ref} type="text" value={item} placeholder="task..."
                 onChange = {e => {onChangeHandle(e.target.value)}} spellCheck={false} 
                 className="h-10 w-72 ssm:w-80 outline-none rounded-md px-4 py-2 my_shadow"
                 />
             </form>
-            <div onClick={() => {addItem(), focusInput(), scrollFocus()}} className="ml-3">
+            <div onClick={() => {addItem(), focusInput()/* , scrollFocus() */}} className="ml-3">
                 <IoIosAdd className="cursor-pointer" size="1.875rem"/>
             </div>
 		</div>
