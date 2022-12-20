2ef4b75b72f416ca39beabf80aaf882f10e49f01
Chu Quang Huy <58379261+Huypip1234@users.noreply.github.com>
2022-12-20 21:20:24 +0700

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import avatar from "../../image/avatar.jpg";
+//detect mobile
+import { isMobile } from "react-device-detect";
 
 const index = () => {
 	return (
 		<div
 			className={`h-20 w-72 ${
-				window.screen.height < 800 ? "hidden" : "flex"
+				isMobile ? "hidden" : "flex"
 			} justify-center bg-white my_shadow rounded-full cursor-pointer`}
 		>
 			<div className="flex w-[15.5rem] justify-between items-center">
